1fe14dd96e39d7d94891f40ad9d5d6a951d43010
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-02-25 12:00:55 +0530

    login & register page function done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+// import Footer from "./components/Footer"
+// import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -9,13 +9,13 @@ import Register from "./pages/Register"
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,22 @@
+import Footer from "../components/Footer"
 import HomePost from "../components/HomePost"
+import Navbar from "../components/Navbar"
 
 
 const Home = () => {
   return (
-    <div className="px-8 md:px-[200px]"> 
+    <>
+    <Navbar/>
+     <div className="px-8 md:px-[200px]"> 
         <HomePost/>
         <HomePost/>
         <HomePost/>
         <HomePost/>
     </div>
+    <Footer/>
+    
+    </>
+   
   )
 }
 
