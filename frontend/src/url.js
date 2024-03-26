8b4d0f09d0aec5497271da105b8373d5d9e24d5e
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-03-26 14:35:57 +0530

    Update url.js
    url updated with host

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000"
-export const IF="http://localhost:5000/images/"
+export const URL="https://trendpulseapi.vercel.app"
+export const IF="https://trendpulseapi.vercel.app/images/"
